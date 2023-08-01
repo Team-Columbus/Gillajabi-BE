b2ff3a5b4fa0a3feaf6c507f93ede095c7e0069d
마재성 <majesoung@naver.com>
2023-08-02 01:29:36 +0900

    feat: 프로필 조회 라우팅 추가 / api url 변경

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from users.views import SignupView, MyTokenObtainPairView, CheckIdExistView
+from users.views import SignupView, MyTokenObtainPairView, CheckIdExistView, MyProfileView
+
 urlpatterns = [
-    path("", SignupView.as_view(), name="signup"),
+    path("signup/", SignupView.as_view(), name="signup"),
     path("login/", MyTokenObtainPairView.as_view(), name="login"),
+    path("profile/", MyProfileView.as_view(), name="profile"),
     path("id/<str:user_id>/exist/", CheckIdExistView.as_view(), name="exist-check"),
     # path('kakao/login/', KakaoLoginView.as_view(), name='kakao_login'),
     # path('kakao/callback/', KakaoCallbackView.as_view(), name='kakao_callback'),
     # path('kakao/login/finish/', KakaoLogin.as_view(), name='kakao_login_todjango'),
-    # path("test/", TestApi.as_view(), name="test")
-    # path("login/", LoginView.as_view(), name="login"),
 ]
