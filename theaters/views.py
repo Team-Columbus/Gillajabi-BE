fd5cd68e83f2b1d08c6c861213ae4258ff9ec975
Pueropstar <majesoung@naver.com>
2023-08-11 16:31:33 +0900

    feat: theaters 테이블 구조를 변경

--- theaters/views.py
+++ theaters/views.py
@@ -8,9 +8,37 @@ from .serializers import CGVMovieSerializer, CGVMovie_DetailSerializer
 @api_view(['GET'])
 def get_top_movies(request):
     movies = CGVMovie.objects.all()
-    random_movies = random.sample(list(movies), 3)  # Pick 3 random movies
-    serializer = CGVMovieSerializer(random_movies, many=True)
-    return Response(serializer.data)
+    random_movies = random.sample(list(movies), 3)
+    top_movies_with_first_time = []
+    
+    for movie in random_movies:
+        if movie.detail.exists():
+            title = movie.title
+            rating = movie.rating
+            poster = movie.movie_poster
+            seat_number = movie.detail.first().seat_number
+            max_seat = movie.detail.first().max_seat
+            start_time = movie.detail.first().start_time  
+            end_time = movie.detail.first().end_time
+
+        movie_data = {
+            "title":title,
+            "rating":rating,
+            "poster":poster,
+            "seat_number" : seat_number,
+            "max_seat": max_seat,
+            "start_time" : start_time,
+            "end_time": end_time
+        }
+        top_movies_with_first_time.append(movie_data)
+
+    return Response(top_movies_with_first_time, status=status.HTTP_200_OK)
+# @api_view(['GET'])
+# def get_top_movies(request):
+#     movies = CGVMovie.objects.all()
+#     random_movies = random.sample(list(movies), 3)  # Pick 3 random movies
+#     serializer = CGVMovieSerializer(random_movies, many=True)
+#     return Response(serializer.data)
 
 @api_view(['GET'])
 def get_movie_list(request):
