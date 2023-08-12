69ae1bdd08b5df232b568a6950f2bf70a5f4430e
마재성 <majesoung@naver.com>
2023-08-12 22:07:30 +0900

    feat: 퀘스트 생성 추가

--- quests/models.py
+++ quests/models.py
@@ -9,6 +9,7 @@ class Quest(models.Model):
 
     content = models.JSONField(verbose_name="퀘스트내용",max_length=300, null=True,blank=True)
     is_do = models.BooleanField(verbose_name="퀘스트수행여부",default=False)
+    is_accept = models.BooleanField(verbose_name="퀘스트수락여부",default=False)
     user = models.ForeignKey(to = User, on_delete=models.CASCADE,related_name="quest")
     
     
--- quests/views.py
+++ quests/views.py
@@ -1,47 +1,66 @@
-import random
+import random, jwt
 from django.shortcuts import render
 from rest_framework.views import APIView
 from django.contrib.auth import get_user_model
-from theaters.models import CGVMovie
+from rest_framework.response import Response
+from theaters.models import CGVMovie, Detail
+from quests.models import Quest
+from mysettings import MY_SECRET_KEY
+
 # Create your views here.
 
 
 User = get_user_model()
 
 
-
 class QuestProvideView(APIView):
+    def post(self, request):
+        access_token = request.data["access_token"]
+        decoded = jwt.decode(access_token, MY_SECRET_KEY, algorithms=["HS256"])
+        user_id = decoded.get("user_id")
 
-    def post(Self,request):
+        user = User.objects.get(id=user_id)
 
-        user_id = request.data["user_id"]
+        quest = generate_quest()
+        Quest.objects.create(content=quest, user=user)
 
-        user = User.objects.get(user_id=user_id)
-
-    
+        return Response(quest)
 
 
 def generate_quest():
+    # categories = ["패스트푸드","영화관","교통"]
 
-    categories = ["패스트푸드","영화관","교통"]
-
-    choice_category = random.choice(categories)
+    # choice_category = random.choice(categories)
+    choice_category = "영화관"
 
     if choice_category == "패스트푸드":
         pass
     elif choice_category == "영화관":
         movies = CGVMovie.objects.all()
 
-        moive_list = [movie.name for movie in movies]
+        movie_list = [movie.title for movie in movies]
 
-        choice_movie = random.choices(moive_list)
+        choice_movie = random.choices(movie_list)[0]
 
-        
+        data = CGVMovie.objects.get(title=choice_movie)
 
+        details = Detail.objects.filter(movie=data)
 
+        time_list = [detail.start_time for detail in details]
 
-    elif choice_category =="교통":
+        choice_time = random.choices(time_list)[0]
+
+        target = {
+            "normal": random.randint(1, 2),
+            "teen": random.randint(0, 2),
+            "disabled": random.randint(0, 2),
+            "silver": random.randint(0, 2),
+        }
+        return {
+            "category": choice_category,
+            "title": choice_movie,
+            "time": choice_time,
+            "ticket": target,
+        }
+    elif choice_category == "고통":
         pass
-    
-    
-
