71f340d44b5cc8da5a6858b9a095cffeb89590b0
마재성 <majesoung@naver.com>
2023-08-17 03:06:19 +0900

    feat: 영화 세부사항 api에서 리턴값 변경 / 불필요한 serializer 제거

--- theaters/serializers.py
+++ theaters/serializers.py
@@ -1,21 +1,18 @@
 from rest_framework import serializers
-from .models import CGVMovie,Detail
+from .models import CGVMovie, Detail
 
-class CGVMovie_DetailSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CGVMovie
-        fields = ['id', 'movie_poster', 'title', 'theater_floor', 'theater_house']
 
 class MovieTopSerializer(serializers.ModelSerializer):
-
     class Meta:
         models = Detail
-        fields="__all__"
+        fields = "__all__"
+
 
 class DetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Detail
-        fields = ["id","seat_number","max_seat","start_time","end_time"]
+        fields = ["id", "seat_number", "max_seat", "start_time", "end_time"]
+
 
 class CGVMovieSerializer(serializers.ModelSerializer):
     detail = DetailSerializer(many=True, read_only=True)  # Include related Detail data
--- theaters/views.py
+++ theaters/views.py
@@ -2,15 +2,16 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 import random
-from .models import CGVMovie
-from .serializers import CGVMovieSerializer, CGVMovie_DetailSerializer
+from .models import CGVMovie, Detail
+from .serializers import CGVMovieSerializer
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 def get_top_movies(request):
     movies = CGVMovie.objects.all()
     random_movies = random.sample(list(movies), 3)
     top_movies_with_first_time = []
-    
+
     for movie in random_movies:
         if movie.detail.exists():
             title = movie.title
@@ -18,40 +19,49 @@ def get_top_movies(request):
             poster = movie.movie_poster
             seat_number = movie.detail.first().seat_number
             max_seat = movie.detail.first().max_seat
-            start_time = movie.detail.first().start_time  
+            start_time = movie.detail.first().start_time
             end_time = movie.detail.first().end_time
 
         movie_data = {
-            "title":title,
-            "rating":rating,
-            "poster":poster,
-            "seat_number" : seat_number,
+            "title": title,
+            "rating": rating,
+            "poster": poster,
+            "seat_number": seat_number,
             "max_seat": max_seat,
-            "start_time" : start_time,
-            "end_time": end_time
+            "start_time": start_time,
+            "end_time": end_time,
         }
         top_movies_with_first_time.append(movie_data)
 
     return Response(top_movies_with_first_time, status=status.HTTP_200_OK)
-# @api_view(['GET'])
-# def get_top_movies(request):
-#     movies = CGVMovie.objects.all()
-#     random_movies = random.sample(list(movies), 3)  # Pick 3 random movies
-#     serializer = CGVMovieSerializer(random_movies, many=True)
-#     return Response(serializer.data)
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 def get_movie_list(request):
     movies = CGVMovie.objects.all()
     serializer = CGVMovieSerializer(movies, many=True)
     return Response(serializer.data)
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 def get_movie_detail(request, pk):
     try:
-        movie = CGVMovie.objects.get(pk=pk)
-    except CGVMovie.DoesNotExist:
-        return Response({'error': 'Movie not found'}, status=status.HTTP_404_NOT_FOUND)
+        detail = Detail.objects.get(id=pk)
+        movie = detail.movie
 
-    serializer = CGVMovie_DetailSerializer(movie)
-    return Response(serializer.data)
+        movie_data = {
+            "id": movie.id,
+            "movie_poster": movie.movie_poster,
+            "title": movie.title,
+            "rating": movie.rating,
+            "theater_house": movie.theater_house,
+            "theater_floor": movie.theater_floor,
+            "seat_number": detail.seat_number,
+            "max_seat": detail.max_seat,
+            "start_time": detail.start_time,
+            "end_time": detail.end_time,
+        }
+    except Detail.DoesNotExist:
+        return Response({"error": "Movie not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    return Response(movie_data)
