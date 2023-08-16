a87b029a2f34f2c9451267a9655174244a95227c
Pueropstar <majesoung@naver.com>
2023-08-17 04:16:20 +0900

    chore: DB 관련 설정 변경

--- config/settings.py
+++ config/settings.py
@@ -110,26 +110,26 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql', # engine: mysql
-#         'NAME' : MY_DB_NAME, # DB Name
-#         'USER' : 'root', # DB User
-#         'PASSWORD' : MY_PASSWORD, # Password
-#         'HOST': MY_HOST, # 생성한 데이터베이스 엔드포인트
-#         'PORT': '3306', # 데이터베이스 포트
-#         'OPTIONS':{
-#             'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'"
-#         }
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', # engine: mysql
+        'NAME' : MY_DB_NAME, # DB Name
+        'USER' : 'root', # DB User
+        'PASSWORD' : MY_PASSWORD, # Password
+        'HOST': MY_HOST, # 생성한 데이터베이스 엔드포인트
+        'PORT': '3306', # 데이터베이스 포트
+        'OPTIONS':{
+            'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
+    }
+}
 
 
 # Password validation
